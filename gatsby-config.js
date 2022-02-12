0ed744b102e5ebec8e25a56130e867b6df234fc9
Kurt Hu <kurthu2@gmail.com>
2022-02-12 14:29:19 -0500

    fix typo in path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  module.exports = {
-    pathPrefix: "/anima",
-  }
+  pathPrefix: "/anima",
 }
